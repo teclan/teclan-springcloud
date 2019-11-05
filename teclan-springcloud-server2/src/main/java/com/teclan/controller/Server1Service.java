57b55fc1027731b4e365e6299a6e31081348f614
teclan <tbj621@163.com>
2019-11-05 11:14:21 +0800

    Bump SpringBoot to 2.x

--- teclan-springcloud-server2/src/main/java/com/teclan/controller/Server1Service.java
+++ teclan-springcloud-server2/src/main/java/com/teclan/controller/Server1Service.java
@@ -1,6 +1,6 @@
 package com.teclan.controller;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
